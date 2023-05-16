365da1b4073dd500995d2ed06d8dd3978bf994a0
anhnt041096 <anhanh04101996@gmail.com>
2023-05-16 22:33:52 +0700

    Integrate apis and display list user

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Container } from 'react-bootstrap';
 import './App.scss';
 import TableUsers from './components/TableUsers';
 import Header from './components/header';
@@ -6,7 +7,9 @@ function App() {
   return (
     <div className="app-container">
       <Header />
-      <TableUsers />
+      <Container>
+        <TableUsers />
+      </Container>
     </div>
   );
 }
--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,9 +1,48 @@
+import { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import { fetchAllUser } from "../services/userService";
+
 const TableUsers = (props) => {
-    return (
-        <>
-            Table..
-        </>
-    )
-}
+  const [listUsers, setListUsers] = useState([]);
+
+  useEffect(() => {
+    // cal apis
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    let res = await fetchAllUser();
+    if (res && res.data) {
+      setListUsers(res.data.data);
+    }
+  };
+
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.email}</td>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </>
+  );
+};
 
 export default TableUsers;
